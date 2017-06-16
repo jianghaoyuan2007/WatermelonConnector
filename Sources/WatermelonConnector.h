f42d664e7de091e3975c16a3e8cc94c0b6b0c0c2
Stephen Chiang <jianghaoyuan2007@gmail.com>
2017-06-16 16:28:14 +0800

    Delete ReplaceMe.h from Sources/WatermelonConnector.h.

--- Sources/WatermelonConnector.h
+++ Sources/WatermelonConnector.h
@@ -16,5 +16,4 @@ FOUNDATION_EXPORT const unsigned char WatermelonConnectorVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <WatermelonConnector/PublicHeader.h>
 
-#import "ReplaceMe.h"
-
+#import "CTMediator+Watermelon.h"
